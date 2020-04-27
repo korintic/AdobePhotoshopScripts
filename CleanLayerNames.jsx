0e2d05c132eb7c91c44710ae51d4e41c9848118f
ohihnala <ohihnala@gmail.com>
2020-04-27 09:06:48 +0300

    Update logic for checking for custom layer names

--- CleanLayerNames.jsx
+++ CleanLayerNames.jsx
@@ -12,7 +12,7 @@ var ignoreTextLayers = true;
 var ignoreShapeFillAndPatternLayers = false;
 var ignoreSmartObjects = false;
 var ignoreGrps = true;
-var ignoreCustomLayerNames = true; //Ignore layer names not starting with the photoshop defaults for that layer type
+var ignoreCustomLayerNames = false; //Ignore layer names not starting with the photoshop defaults for that layer type
 
 // Add opacity or fill amount as a suffix to layer name
 // If both are set to "true" "O:" and "F:" are added in front of the percentage
@@ -23,9 +23,11 @@ String.prototype.startsWith = function (str) {
     return this.substring(0, str.length) === str;
 }
 
-var adjustmentLayerDefaultNames = ["Brightness/Contrast", "Levels", "Curves", "Exposure", "Vibrance", "Hue/Saturation", "Color Balance", "Black & White", "Photo Filter", "Channel Mixer", "Color Lookup", "Invert", "Posterize", "Threshold", "Gradient Map", "Selective Color"];
+var adjustmentLayerDefaultNames = ["Brightness/Contrast", "Levels", "Curves", "Exposure", "Vibrance", "Hue/Saturation", "Invert", "Posterize", "Threshold"];
+var adjustmentLayerLongDefaulNames = ["Color Balance", "Photo Filter", "Channel Mixer", "Color Lookup", "Gradient Map", "Selective Color"];
 
-var shapeLayerDefaultNames = ["Color Fill","Gradient Fill","Pattern Fill", "Rectangle", "Rounded Rectangle", "Ellipse","Polygon", "Shape"];
+var shapeLayerDefaultNames = ["Rectangle", "Ellipse","Polygon", "Shape"];
+var shapeLayerLongDefaultNames = ["Color Fill","Gradient Fill","Pattern Fill", "Rectangle", "Rounded Rectangle", "Ellipse","Polygon", "Shape"];
 
 var adjustmentLayer = {
     "BRIGHTNESSCONTRAST": LayerKind.BRIGHTNESSCONTRAST,
@@ -86,15 +88,31 @@ function CleanLayerNames() {
             else if(addFillAmount) {
                 suffix = Math.round(doc.activeLayer.fillOpacity) + "%";
             }
-            var nameStart = (doc.activeLayer.name.split(" ")[0]+" "+doc.activeLayer.name.split(" ")[1]);
-            if((!doc.activeLayer.name.startsWith("Layer") && doc.activeLayer.kind === LayerKind.NORMAL) && ignoreCustomLayerNames) {
-                continue;
-            }
-            if((!(new RegExp(shapeLayerDefaultNames.join("|")).test(nameStart)) && shapeFillAndPatternLayer[doc.activeLayer.kind.toString().split(".")[1]] !== undefined) && ignoreCustomLayerNames) {
-                continue;
-            }
-            if((!(new RegExp(adjustmentLayerDefaultNames.join("|")).test(nameStart)) && adjustmentLayer[doc.activeLayer.kind.toString().split(".")[1]] !== undefined) && ignoreCustomLayerNames) {
-                continue;
+            var isCustomLayerName = true;
+            if(ignoreCustomLayerNames) {
+                var nameStart = (doc.activeLayer.name.split(" ")[0]);
+                var longNameStart = (doc.activeLayer.name.split(" ")[0]+" "+doc.activeLayer.name.split(" ")[1]);
+                if((doc.activeLayer.name.startsWith("Layer") && doc.activeLayer.kind === LayerKind.NORMAL)) {
+                    isCustomLayerName = false;
+                }
+                if((new RegExp(shapeLayerLongDefaultNames.join("|")).test(longNameStart)) && shapeFillAndPatternLayer[doc.activeLayer.kind.toString().split(".")[1]] !== undefined) {
+                    isCustomLayerName = false;
+                }
+                else if((new RegExp(shapeLayerDefaultNames.join("|")).test(nameStart)) && shapeFillAndPatternLayer[doc.activeLayer.kind.toString().split(".")[1]] !== undefined) {
+                    isCustomLayerName = false;
+                }
+                if((new RegExp(adjustmentLayerLongDefaulNames.join("|")).test(longNameStart)) && adjustmentLayer[doc.activeLayer.kind.toString().split(".")[1]] !== undefined) {
+                    isCustomLayerName = false;
+                }
+                else if((new RegExp(adjustmentLayerDefaultNames.join("|")).test(nameStart)) && adjustmentLayer[doc.activeLayer.kind.toString().split(".")[1]] !== undefined) {
+                    isCustomLayerName = false;
+                }
+                else if((doc.activeLayer.name.startsWith("Black & White") && adjustmentLayer[doc.activeLayer.kind.toString().split(".")[1]] !== undefined)) {
+                    isCustomLayerName = false;
+                }
+                if(isCustomLayerName) {
+                    continue;
+                }
             }
             if(adjustmentLayer[doc.activeLayer.kind.toString().split(".")[1]] !== undefined && ignoreAdjustementLayers){
                 continue;
@@ -121,335 +139,336 @@ function upperCaseFirstLetter(string) {
 }
 
 function SetActiveLayerColor(color) {
-    var idsetd = charIDToTypeID( "setd" );
+    var idsetd = charIDToTypeID("setd");
     var desc = new ActionDescriptor();
-    var idnull = charIDToTypeID( "null" );
+    var idnull = charIDToTypeID("null");
     var ref = new ActionReference();
-    var idLyr = charIDToTypeID( "Lyr " );
-    var idOrdn = charIDToTypeID( "Ordn" );
-    var idTrgt = charIDToTypeID( "Trgt" );
-    ref.putEnumerated( idLyr, idOrdn, idTrgt );
-    desc.putReference( idnull, ref );
-    var idT = charIDToTypeID( "T   " );
+    var idLyr = charIDToTypeID("Lyr ");
+    var idOrdn = charIDToTypeID("Ordn");
+    var idTrgt = charIDToTypeID("Trgt");
+    ref.putEnumerated(idLyr, idOrdn, idTrgt);
+    desc.putReference(idnull, ref);
+    var idT = charIDToTypeID("T   ");
     var desc2 = new ActionDescriptor();
-    var idClr = charIDToTypeID( "Clr " );var idClr = charIDToTypeID( "Clr " );
-    var idBl = charIDToTypeID( color );
-    desc2.putEnumerated( idClr, idClr, idBl );
-    var idLyr = charIDToTypeID( "Lyr " );
-    desc.putObject( idT, idLyr, desc2 );
-    executeAction( idsetd, desc, DialogModes.NO )
+    var idClr = charIDToTypeID("Clr ");
+    var idClr = charIDToTypeID("Clr ");
+    var idBl = charIDToTypeID(color);
+    desc2.putEnumerated(idClr, idClr, idBl);
+    var idLyr = charIDToTypeID("Lyr ");
+    desc.putObject(idT, idLyr, desc2);
+    executeAction(idsetd, desc, DialogModes.NO);
 }
 
-function getSelectedLayersIndices(){
+function getSelectedLayersIndices() {
     var selectedLayers = new Array;
     var ref = new ActionReference();
-    ref.putEnumerated( charIDToTypeID("Dcmn"), charIDToTypeID("Ordn"), charIDToTypeID("Trgt") );
+    ref.putEnumerated(charIDToTypeID("Dcmn"), charIDToTypeID("Ordn"), charIDToTypeID("Trgt"));
     var desc = executeActionGet(ref);
-    if( desc.hasKey( stringIDToTypeID( 'targetLayers' ) ) ){
-       desc = desc.getList( stringIDToTypeID( 'targetLayers' ));
-        var c = desc.count 
+    if (desc.hasKey(stringIDToTypeID('targetLayers'))) {
+        desc = desc.getList(stringIDToTypeID('targetLayers'));
+        var c = desc.count;
         var selectedLayers = new Array();
-        for(var i=0;i<c;i++){
-          try{ 
-             activeDocument.backgroundLayer;
-             selectedLayers.push(  desc.getReference( i ).getIndex() );
-          }catch(e){
-             selectedLayers.push(  desc.getReference( i ).getIndex()+1 );
-          }
+        for (var i = 0; i < c; i++) {
+            try {
+                activeDocument.backgroundLayer;
+                selectedLayers.push(desc.getReference(i).getIndex());
+            } catch (e) {
+                selectedLayers.push(desc.getReference(i).getIndex() + 1);
+            }
         }
-     }else{
-       var ref = new ActionReference(); 
-       ref.putProperty( charIDToTypeID("Prpr") , charIDToTypeID( "ItmI" )); 
-       ref.putEnumerated( charIDToTypeID("Lyr "), charIDToTypeID("Ordn"), charIDToTypeID("Trgt") );
-       try{ 
-          activeDocument.backgroundLayer;
-          selectedLayers.push( executeActionGet(ref).getInteger(charIDToTypeID( "ItmI" ))-1);
-       }catch(e){
-          selectedLayers.push( executeActionGet(ref).getInteger(charIDToTypeID( "ItmI" )));
-       }
+    } else {
+        var ref = new ActionReference();
+        ref.putProperty(charIDToTypeID("Prpr"), charIDToTypeID("ItmI"));
+        ref.putEnumerated(charIDToTypeID("Lyr "), charIDToTypeID("Ordn"), charIDToTypeID("Trgt"));
+        try {
+            activeDocument.backgroundLayer;
+            selectedLayers.push(executeActionGet(ref).getInteger(charIDToTypeID("ItmI")) - 1);
+        } catch (e) {
+            selectedLayers.push(executeActionGet(ref).getInteger(charIDToTypeID("ItmI")));
+        }
     }
     return selectedLayers;
- }
+}
 
-function selectLayerByIndex(index,add){ 
+function selectLayerByIndex(index, add) {
     var ref = new ActionReference();
     ref.putIndex(charIDToTypeID("Lyr "), index);
     var desc = new ActionDescriptor();
-    desc.putReference(charIDToTypeID("null"), ref );
-    if(add) {
-        desc.putEnumerated( stringIDToTypeID( "selectionModifier" ), stringIDToTypeID( "selectionModifierType" ), stringIDToTypeID( "addToSelection" ) ) 
+    desc.putReference(charIDToTypeID("null"), ref);
+    if (add) {
+        desc.putEnumerated(stringIDToTypeID("selectionModifier"), stringIDToTypeID("selectionModifierType"), stringIDToTypeID("addToSelection"))
     }
-    desc.putBoolean( charIDToTypeID( "MkVs" ), false ); 
-    try{
-        executeAction(charIDToTypeID("slct"), desc, DialogModes.NO );
-    }catch(e){}
+    desc.putBoolean(charIDToTypeID("MkVs"), false);
+    try {
+        executeAction(charIDToTypeID("slct"), desc, DialogModes.NO);
+    } catch (e) {}
 }
 
 function CleanBlendModeName(str) {
-    switch(String(str)){
+    switch (String(str)) {
         case "Colorburn":
-        str= "Color Burn"
-        break;
-       
+            str = "Color Burn";
+            break;
+
         case "Linearburn":
-        str= "Linear Burn"
-        break;
-        
+            str = "Linear Burn";
+            break;
+
         case "Darkercolor":
-        str= "Darker Color"
-        break;
-        
+            str = "Darker Color";
+            break;
+
         case "Colordodge":
-        str= "Color Dodge"
-        break;4
-       
+            str = "Color Dodge";
+            break;
+            4
+
         case "Lineardodge":
-        str = "Linear Dodge (Add)"
-        break;
-        
+            str = "Linear Dodge (Add)";
+            break;
+
         case "Lightercolor":
-        str= "Lighter Color"
-        break;
+            str = "Lighter Color";
+            break;
 
         case "Softlight":
-        str= "Soft Light"
-        break;
-        
+            str = "Soft Light";
+            break;
+
         case "Hardlight":
-        str= "Hard Light"
-        break;
-        
+            str = "Hard Light";
+            break;
+
         case "Vividlight":
-        str= "Vivid Light"
-        break;
-        
+            str = "Vivid Light";
+            break;
+
         case "Linearlight":
-        str= "Linearr Light"
-        break;4
-        
+            str = "Linearr Light";
+            break;
+            4
+
         case "Pinlight":
-        str = "Pin Light"
-        break;
-        
+            str = "Pin Light";
+            break;
+
         case "Hardmix":
-        str= "Hard Mix"
-        break;
+            str = "Hard Mix";
+            break;
 
         case "Colorblend":
-        str= "Color"
-        break;
+            str = "Color";
+            break;
     }
     return str;
 }
 
 function SetLayerColorToBlendMode(str) {
-    switch(String(str)){
+    switch (String(str)) {
         // Color names: "None",  "Rd  ", "Orng", "Ylw ", "Grn ", "Bl  ",  "Vlt ", "Gry "
         // None "None"
         case "NORMAL":
-        SetActiveLayerColor("None")
-        break;
+            SetActiveLayerColor("None");
+            break;
 
-        // Grey "Gry "
+            // Grey "Gry "
         case "DISSOLVE":
-        SetActiveLayerColor("Gry ")
-        break;
+            SetActiveLayerColor("Gry ");
+            break;
 
-        // Blue "Bl  "
+            // Blue "Bl  "
         case "MULTIPLY":
-        SetActiveLayerColor("Bl  ")
-        break;
+            SetActiveLayerColor("Bl  ");
+            break;
 
         case "DARKEN":
-        SetActiveLayerColor("Bl  ")
-        break;
+            SetActiveLayerColor("Bl  ");
+            break;
 
         case "COLORBURN":
-        SetActiveLayerColor("Bl  ")
-        break;
+            SetActiveLayerColor("Bl  ");
+            break;
 
         case "LINEARBURN":
-        SetActiveLayerColor("Bl  ")
-        break;
+            SetActiveLayerColor("Bl  ");
+            break;
 
         case "DARKERCOLOR":
-        SetActiveLayerColor("Bl  ")
-        break;
+            SetActiveLayerColor("Bl  ");
+            break;
 
-        // Yellow "Ylw "
+            // Yellow "Ylw "
         case "LIGHTEN":
-        SetActiveLayerColor("Ylw ")
-        break;
+            SetActiveLayerColor("Ylw ");
+            break;
 
         case "SCREEN":
-        SetActiveLayerColor("Ylw ")
-        break;
+            SetActiveLayerColor("Ylw ");
+            break;
 
         case "COLORDODGE":
-        SetActiveLayerColor("Ylw ")
-        break;
+            SetActiveLayerColor("Ylw ");
+            break;
 
         case "LINEARDODGE":
-        SetActiveLayerColor("Ylw ")
-        break;
+            SetActiveLayerColor("Ylw ");
+            break;
 
         case "LIGHTERCOLOR":
-        SetActiveLayerColor("Ylw ")
-        break;
+            SetActiveLayerColor("Ylw ");
+            break;
 
-        // Orange "Orng"
+            // Orange "Orng"
         case "OVERLAY":
-        SetActiveLayerColor("Orng")
-        break;
+            SetActiveLayerColor("Orng");
+            break;
 
         case "SOFTLIGHT":
-        SetActiveLayerColor("Orng")
-        break;
+            SetActiveLayerColor("Orng");
+            break;
 
         case "HARDLIGHT":
-        SetActiveLayerColor("Orng")
-        break;
+            SetActiveLayerColor("Orng");
+            break;
 
         case "VIVIDLIGHT":
-        SetActiveLayerColor("Orng")
-        break;
+            SetActiveLayerColor("Orng");
+            break;
 
         case "LINEARLIGHT":
-        SetActiveLayerColor("Orng")
-        break;
+            SetActiveLayerColor("Orng");
+            break;
 
         case "PINLIGHT":
-        SetActiveLayerColor("Orng")
-        break;
+            SetActiveLayerColor("Orng");
+            break;
 
         case "HARDMIX":
-        SetActiveLayerColor("Orng")
-        break;
+            SetActiveLayerColor("Orng");
+            break;
 
-        // Green "Grn "
+            // Green "Grn "
         case "DIFFERENCE":
-        SetActiveLayerColor("Grn ")
-        break;
+            SetActiveLayerColor("Grn ");
+            break;
 
         case "EXCLUSION":
-        SetActiveLayerColor("Grn ")
-        break;
+            SetActiveLayerColor("Grn ");
+            break;
 
         case "SUBSTACT":
-        SetActiveLayerColor("Grn ")
-        break;
+            SetActiveLayerColor("Grn ");
+            break;
 
         case "DIVIDE":
-        SetActiveLayerColor("Grn ")
-        break;
+            SetActiveLayerColor("Grn ");
+            break;
 
-        // Violet "Vlt "
+            // Violet "Vlt "
         case "HUE":
-        SetActiveLayerColor("Vlt ")
-        break;
+            SetActiveLayerColor("Vlt ");
+            break;
 
         case "SATURATION":
-        SetActiveLayerColor("Vlt ")
-        break;
+            SetActiveLayerColor("Vlt ");
+            break;
 
         case "COLORBLEND":
-        SetActiveLayerColor("Vlt ")
-        break;
+            SetActiveLayerColor("Vlt ");
+            break;
 
         case "LUMINOSITY":
-        SetActiveLayerColor("Vlt ")
-        break;
+            SetActiveLayerColor("Vlt ");
+            break;
     }
 }
 
 function SetLayerColorByLayerType(str) {
-    switch(String(str)){
+    switch (String(str)) {
         // Color names: "None",  "Rd  ", "Orng", "Ylw ", "Grn ", "Bl  ",  "Vlt ", "Gry "
         case "NORMAL":
-        // SetActiveLayerColor("None")
-        break;
+            // SetActiveLayerColor("None");*
+            break;
 
-        // Grey "Gry "
         case "TEXT":
-        // SetActiveLayerColor("None")
-        break;
+            // SetActiveLayerColor("None");
+            break;
 
-        // Blue "Bl  "
         case "SMARTOBJECT":
-        // SetActiveLayerColor("None")
-        break;
+            // SetActiveLayerColor("None");
+            break;
 
         case "SOLIDFILL":
-        // SetActiveLayerColor("None")
-        break;
+            // SetActiveLayerColor("None");
+            break;
 
         case "GRADIENTFILL":
-        // SetActiveLayerColor("None")
-        break;
+            // SetActiveLayerColor("None");
+            break;
 
         case "PATTERNFILL":
-        // SetActiveLayerColor("None")
-        break;
+            // SetActiveLayerColor("None");
+            break;
 
-        // All adjustment layers layer colors are set to red
+            // All adjustment layers layer colors are set to red
         case "BRIGHTNESSCONTRAST":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
 
         case "LEVELS":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
 
         case "CURVES":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
 
         case "EXPOSURE":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
 
         case "VIBRANCE":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
 
         case "HUESATURATION":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
 
         case "COLORBALANCE":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
 
         case "BLACKANDWHITE":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
 
         case "CHANNELMIXER":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
 
         case "COLORLOOKUP":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
 
         case "INVERSION":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
 
         case "POSTERIZE":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
 
         case "THRESHOLD":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
 
         case "GRADIENTMAP":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
 
         case "SELECTIVECOLOR":
-        SetActiveLayerColor("Rd  ")
-        break;
+            SetActiveLayerColor("Rd  ");
+            break;
     }
 }
